351e23b949f2cbdaa3e8b3920246fbbcab6f31d6
Ghanbar <ghanbar.naruto.78@gmail.com>
2023-09-09 02:53:02 +0330

    refactor: change period selector styles and define types

--- src/components/PagesTop.tsx
+++ src/components/PagesTop.tsx
@@ -1,20 +1,66 @@
 'use client';
-import React, { useState } from 'react';
+import React, { FC, useState } from 'react';
 
-import { MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import CalendarMonthTwoToneIcon from '@mui/icons-material/CalendarMonthTwoTone';
+import { MenuItem, TextField, InputAdornment } from '@mui/material';
 import { usePathname } from 'next/navigation';
 
-const PERIODS = [
+type Period = {
+  label: string;
+  value: string;
+};
+
+const PERIODS: Array<Period> = [
   { label: 'Last week', value: '1' },
   { label: 'Last 2 week', value: '2' },
   { label: 'Last month', value: '3' },
 ];
+
+interface PeriodSelectorProps {
+  period: Period['value'];
+  onPeriodChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const PeriodSelector: FC<PeriodSelectorProps> = ({
+  period,
+  onPeriodChange,
+}) => {
+  return (
+    <TextField
+      select
+      value={period}
+      onChange={onPeriodChange}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <CalendarMonthTwoToneIcon fontSize="small" />
+          </InputAdornment>
+        ),
+      }}
+      size="small"
+      SelectProps={{
+        classes: { select: 'text-title-small py-2' },
+        MenuProps: { classes: { list: 'p-0' } },
+      }}
+      sx={{ width: '150px' }}
+    >
+      {PERIODS.map(({ label, value }) => {
+        return (
+          <MenuItem key={label} value={value} className="text-title-small">
+            {label}
+          </MenuItem>
+        );
+      })}
+    </TextField>
+  );
+};
+
 const PagesTop = () => {
   const pathname = usePathname();
   const [period, setPeriod] = useState(PERIODS[0].value);
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setPeriod(event.target.value as string);
+  const onPeriodChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPeriod(event.target.value);
   };
 
   // also, we can use an object for mapping every url address to a title; instead of using split function
@@ -23,15 +69,7 @@ const PagesTop = () => {
       <span className="text-title-large capitalize">
         {pathname?.split('/').at(1) || ''}
       </span>
-      <Select value={period} onChange={handleChange} displayEmpty>
-        {PERIODS.map(({ label, value }) => {
-          return (
-            <MenuItem key={label} value={value}>
-              {label}
-            </MenuItem>
-          );
-        })}
-      </Select>
+      <PeriodSelector period={period} onPeriodChange={onPeriodChange} />
     </header>
   );
 };
